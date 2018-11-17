c64ed1554e45ab1383a0e2f642f15b43cc61e16e
DarkSideMaster <39058641+DarkSideMaster@users.noreply.github.com>
2018-11-17 22:15:56 +0200

    Update OrdersController.cs
    test

--- Test_Uklon_WebApplication/Controllers/OrdersController.cs
+++ Test_Uklon_WebApplication/Controllers/OrdersController.cs
@@ -108,4 +108,4 @@ namespace Test_Uklon_WebApplication.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
-}
+} 
